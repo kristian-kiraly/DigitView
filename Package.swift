8c894103f1d5e3eba476ad6b9d91811e8c32a03f
Kristian Kiraly <kristian.kiraly@gmail.com>
2023-10-26 22:35:27 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DigitView",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DigitView",
+            targets: ["DigitView"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DigitView"),
+        .testTarget(
+            name: "DigitViewTests",
+            dependencies: ["DigitView"]),
+    ]
+)
